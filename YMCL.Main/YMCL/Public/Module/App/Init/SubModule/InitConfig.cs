9b3841ac6aefd4755db52a1014d7730ab13bbfc5
Star-Dust0 <wlhxc2010@qq.com>
2026-06-12 22:18:39 +0800

    fix: 已有配置时自动迁移旧默认 .minecraft 路径到程序目录

--- YMCL.Main/YMCL/Public/Module/App/Init/SubModule/InitConfig.cs
+++ YMCL.Main/YMCL/Public/Module/App/Init/SubModule/InitConfig.cs
@@ -50,6 +50,25 @@ public static class InitConfig
             File.WriteAllText(ConfigPath.SettingDataPath,
                 JsonConvert.SerializeObject(setting1, Formatting.Indented));
         }
+        else
+        {
+            var folders = JsonConvert.DeserializeObject<List<MinecraftFolder>>(
+                File.ReadAllText(ConfigPath.MinecraftFolderDataPath))!;
+            if (folders.Count > 0 && folders[0].Path?.StartsWith(ConfigPath.UserDataRootPath) == true)
+            {
+                var exeDir = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly()!.Location)!;
+                var newPath = Path.Combine(exeDir, ".minecraft");
+                IO.Disk.Setter.TryCreateFolder(newPath);
+                folders[0] = new MinecraftFolder { Name = folders[0].Name, Path = newPath };
+                File.WriteAllText(ConfigPath.MinecraftFolderDataPath,
+                    JsonConvert.SerializeObject(folders, Formatting.Indented));
+                var setting1 =
+                    JsonConvert.DeserializeObject<SettingEntry>(File.ReadAllText(ConfigPath.SettingDataPath));
+                setting1!.MinecraftFolder = folders[0];
+                File.WriteAllText(ConfigPath.SettingDataPath,
+                    JsonConvert.SerializeObject(setting1, Formatting.Indented));
+            }
+        }
 
         if (!File.Exists(ConfigPath.JavaDataPath))
             File.WriteAllText(ConfigPath.JavaDataPath,
